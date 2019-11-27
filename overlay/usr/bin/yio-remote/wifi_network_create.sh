499dadb03ff860932b07b5be0868c74dbfe8e4f3
Markus Zehnder <business@markuszehnder.ch>
2019-11-27 12:41:23 +0100

    Allow wpa_supplicant to overwrite cfg file
    Part of remote-software#322

--- overlay/usr/bin/yio-remote/wifi_network_create.sh
+++ overlay/usr/bin/yio-remote/wifi_network_create.sh
@@ -1,26 +1,14 @@
 #!/bin/bash
 
-#check if there is a file
 mkdir -p /etc/wpa_supplicant
-if [ -e /etc/wpa_supplicant/wpa_supplicant-wlan0.conf ]; then
-
-    # re-create a configuration file
-    echo "ctrl_interface=/var/run/wpa_supplicant
+# create a configuration file
+# See: https://w1.fi/cgit/hostap/plain/wpa_supplicant/wpa_supplicant.conf
+echo "ctrl_interface=/var/run/wpa_supplicant
 ap_scan=1
+update_config=1
 
 network={
     key_mgmt=WPA-PSK
     ssid="\"$1\""
     psk="\"$2\""
 }" > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-else
-    # create a configuration file
-    echo "ctrl_interface=/var/run/wpa_supplicant
-ap_scan=1
-    
-network={
-    key_mgmt=WPA-PSK
-    ssid="\"$1\""
-    psk="\"$2\""
-}" > /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-fi
