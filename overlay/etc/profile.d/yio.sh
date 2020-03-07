63e13ec1544f2e98353313cbb6098a7ee873f4fb
Markus Zehnder <business@markuszehnder.ch>
2020-03-08 00:07:55 +0100

    Added YIO env variables
    The version env var is replaced in the Buildroot build from the Git
    version tag (a release must have a tag) or read from an optional version
     file in project root directory.
    
    This closes #33

---
+++ overlay/etc/profile.d/yio.sh
@@ -0,0 +1,7 @@
+export YIO_HOME=/usr/bin/yio-remote
+# remote-os release, set during build
+export YIO_OS_VERSION=$BUILD_VERSION
+# Git hash of the remote-os repo, set during build
+export YIO_OS_GITHASH=$GIT_HASH
+export YIO_LOG_DIR=/var/log
+export YIO_LOG_DIR_UPDATE=/boot/log
---
+++ rpi0/git-version.sh
@@ -0,0 +1,42 @@
+#!/bin/sh
+#
+# Build version retrieval script.
+# Prints the current version or 'UNKNOWN' if version could not be determined.
+#
+# 1. see if there is a version file (provided as first argument)
+# 2. then try git-describe to retrieve version tag
+#    - append '-dirty' if local changes are found
+# 3. otherwise fallback: 'UNKNOWN'
+#
+# Inspired by:
+# https://git.kernel.org/pub/scm/git/git.git/tree/GIT-VERSION-GEN?id=HEAD
+
+VERSION_FILE=$1
+DEF_VER=UNKNOWN
+LF='
+'
+
+if test -f "$VERSION_FILE"
+then
+	BUILD_VERSION=$(cat "$VERSION_FILE") || BUILD_VERSION="$DEF_VER"
+elif BUILD_VERSION=$(git describe --match "v[0-9]*" --tags HEAD 2>/dev/null) &&
+	case "$BUILD_VERSION" in
+	*$LF*) (exit 1) ;;
+	v[0-9]*)
+	    # added or modified files? Mark it dirty! (New files are not considered)
+		git update-index -q --refresh
+		test -z "$(git diff-index --name-only HEAD --)" || BUILD_VERSION="$BUILD_VERSION-dirty" ;;
+	esac
+then
+    # here we could adjust the version from the Git tag. E.g. replace dash with dot
+	# BUILD_VERSION=$(echo "$BUILD_VERSION" | sed -e 's/-/./g');
+	:
+else
+    # TODO add version logic for develop and feature branches?
+	BUILD_VERSION="$DEF_VER"
+fi
+
+# strip leading 'v'
+BUILD_VERSION=$(expr "$BUILD_VERSION" : v*'\(.*\)')
+
+echo $BUILD_VERSION
