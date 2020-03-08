fc223e99a986f797fdde96e47c773ce08a5ec75f
Markus Zehnder <business@markuszehnder.ch>
2020-03-08 11:22:26 +0100

    fixup! Added YIO env variables

--- rpi0/git-version.sh
+++ rpi0/git-version.sh
@@ -6,6 +6,12 @@
 # 1. see if there is a version file (provided as first argument)
 # 2. then try git-describe to retrieve version tag
 #    - append '-dirty' if local changes are found
+#    - see: https://git-scm.com/docs/git-describe
+#    Examples:
+#    - build from a version tag: v0.2.0
+#    - build from a develop branch which is 16 commits ahead of the latest
+#      version tag: v0.2.0-16-g60e1688
+#    - build with local modifications: v0.2.0-16-g60e1688-dirty
 # 3. otherwise fallback: 'UNKNOWN'
 #
 # Inspired by:
@@ -19,6 +25,8 @@ LF='
 if test -f "$VERSION_FILE"
 then
 	BUILD_VERSION=$(cat "$VERSION_FILE") || BUILD_VERSION="$DEF_VER"
+# get version from last release tag (e.g. v0.2.0)
+# see https://git-scm.com/docs/git-describe
 elif BUILD_VERSION=$(git describe --match "v[0-9]*" --tags HEAD 2>/dev/null) &&
 	case "$BUILD_VERSION" in
 	*$LF*) (exit 1) ;;
