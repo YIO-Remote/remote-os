ab84867b3187134dfc522406629b1ba8fe7d2e59
Markus Zehnder <business@markuszehnder.ch>
2020-03-18 21:29:23 +0100

    Clean wificopy marker in consecutive startups
    The wpa_supplicant configuration file in /boot might have been removed.
    In this case the wificopy marker no longer applies and needs to be removed.

--- overlay/opt/yio/scripts/wifi-copy-config.sh
+++ overlay/opt/yio/scripts/wifi-copy-config.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
+
 if [ -e /boot/wpa_supplicant.conf ]
 then
+    # TODO check if config files differ or not: skip if same!
+
     echo "Using provided wpa config in /boot."
 
     # stop wifi
@@ -9,7 +12,6 @@ then
     # copy config file
     mkdir -p /etc/wpa_supplicant
     cp /boot/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
-    sleep 5
     # restart wifi
     systemctl start wpa_supplicant@wlan0.service
     sleep 5
@@ -17,4 +19,5 @@ then
     touch /wificopy
 else
     echo "No wpa config in /boot."
+    rm -f  /wificopy
 fi
