79c31c7e90620f5de9b95b71e9825ba397d226b3
Markus Zehnder <business@markuszehnder.ch>
2020-09-17 20:47:12 +0200

    build: Clean up systemd services

--- rpi0/post-build.sh
+++ rpi0/post-build.sh
@@ -12,18 +12,6 @@ if [ -e ${TARGET_DIR}/etc/inittab ]; then
 tty1::respawn:/sbin/getty -L  tty1 0 vt100 # HDMI console' ${TARGET_DIR}/etc/inittab
 fi
 
-echo "Creating systemd services"
-ln -fs ../../../../usr/lib/systemd/system/wpa_supplicant@.service $1/etc/systemd/system/multi-user.target.wants/wpa_supplicant@wlan0.service
-ln -fs ../../../../usr/lib/systemd/system/backlight.service $1/etc/systemd/system/multi-user.target.wants/backlight.service
-ln -fs ../../../../usr/lib/systemd/system/sharp-init.service $1/etc/systemd/system/multi-user.target.wants/sharp-init.service
-ln -fs ../../../../usr/lib/systemd/system/app.service $1/etc/systemd/system/multi-user.target.wants/app.service
-
-
-rm -rf $1/var/log/journal
-
-#rm -r $1/etc/systemd/system/sysinit.target.wants/systemd-timesyncd.service
-#rm -r $1/etc/systemd/system/multi-user.target.wants/dhcpcd.service
-
 # Determine build version
 BUILD_VERSION=$("$SCRIPT_DIR/git-version.sh" "$BR2_EXTERNAL/version")
 
