791c9f38f8cfea8259418a5049c1e5d618c59784
Markus Zehnder <business@markuszehnder.ch>
2020-09-24 21:10:46 +0200

    chore: Refactored Buildroot custom package location
    Created package directory for all future Buildroot custom packages.
    Relocated yio-remote packages.

--- external.mk
+++ external.mk
@@ -5,4 +5,4 @@
 SKIP_BUILD_IMAGE=n
 export SKIP_BUILD_IMAGE
 
-include ${BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH}/yio-remote/yio-remote.mk
+include ${BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH}/package/yio-remote/yio-remote.mk
