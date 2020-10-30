5160094128c1c8820dcffc873c4cb3830ca74905
Markus Zehnder <business@markuszehnder.ch>
2020-10-30 19:38:46 +0100

    build: Simplified custom Buildroot package handling

--- external.mk
+++ external.mk
@@ -5,5 +5,4 @@
 SKIP_BUILD_IMAGE=n
 export SKIP_BUILD_IMAGE
 
-include ${BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH}/package/yio-remote/yio-remote.mk
-include ${BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH}/package/qtmqtt/qtmqtt.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH)/package/*/*.mk))
--- package/yio-remote/yio-remote.mk
+++ package/yio-remote/yio-remote.mk
@@ -7,15 +7,4 @@
 # Variable for other YIO packages to use
 YIO_REMOTE_DEBUG = $(call qstrip,$(BR2_PACKAGE_YIO_REMOTE_DEBUG))
 
-include ../package/yio-remote/yio-remote-software/yio-remote-software.mk
-include ../package/yio-remote/yio-integrations-library/yio-integrations-library.mk
-include ../package/yio-remote/yio-web-configurator/yio-web-configurator.mk
-
-include ../package/yio-remote/yio-integration-dock/yio-integration-dock.mk
-include ../package/yio-remote/yio-integration-homeassistant/yio-integration-homeassistant.mk
-include ../package/yio-remote/yio-integration-homey/yio-integration-homey.mk
-include ../package/yio-remote/yio-integration-spotify/yio-integration-spotify.mk
-include ../package/yio-remote/yio-integration-bangolufsen/yio-integration-bangolufsen.mk
-include ../package/yio-remote/yio-integration-openhab/yio-integration-openhab.mk
-include ../package/yio-remote/yio-integration-openweather/yio-integration-openweather.mk
-include ../package/yio-remote/yio-integration-roon/yio-integration-roon.mk
+include $(sort $(wildcard $(BR2_EXTERNAL_BUILDROOT_SUBMODULE_PATH)/package/yio-remote/*/*.mk))
