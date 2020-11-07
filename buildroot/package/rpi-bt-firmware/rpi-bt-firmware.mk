f89c81faa53e1b610d3cd12178e12f53d7cbb8a8
Markus Zehnder <business@markuszehnder.ch>
2020-11-07 13:39:00 +0100

    feat: Update RPi BT firmware a4e08822e3f24a6211f6ac94bc98b7ef87700c70
    Signed-off-by: Markus Zehnder <business@markuszehnder.ch>

--- buildroot/package/rpi-bt-firmware/rpi-bt-firmware.mk
+++ buildroot/package/rpi-bt-firmware/rpi-bt-firmware.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RPI_BT_FIRMWARE_VERSION = 929b7af9c43e7edc8b289435a9981b240f47b218
+RPI_BT_FIRMWARE_VERSION = a4e08822e3f24a6211f6ac94bc98b7ef87700c70
 RPI_BT_FIRMWARE_SITE = $(call github,RPi-Distro,bluez-firmware,$(RPI_BT_FIRMWARE_VERSION))
 RPI_BT_FIRMWARE_LICENSE = PROPRIETARY
 RPI_BT_FIRMWARE_LICENSE_FILES = broadcom/BCM-LEGAL.txt
